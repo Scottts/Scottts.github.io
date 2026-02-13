501255fb43f87a370e352a8831916981f58b8408
Scottts <Scottts@users.noreply.github.com>
2026-02-13 09:46:42 +0000

    deploy: 925323c844c896a2b7a9d14d72e9ac001c2f5ef2

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ae50bf8d.js',
+                path_to_root + 'searchindex-9cc2bf69.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
